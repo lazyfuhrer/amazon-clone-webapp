30ebda90c01bace6aef658930c2ca6036e480e25
Biswarghya Biswas <iambiswarghya@gmail.com>
2022-04-27 23:26:10 +0530

    Cart

---
+++ src/slices/cartSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      state.items = [...state.items, action.payload]
+    },
+    removeFromCart: (state, action) => {
+      const index = state.items.findIndex((cartItem) => cartItem.id === action.payload.id);
+
+      let newCart = [...state.items];
+
+      if (index >= 0) {
+        newCart.splice(index, 1)
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.payload.id}) as it's not in the cart.`
+        );
+      }
+
+      state.items = newCart;
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+
+// Selectors - This is how we pull information from the Global store slice
+export const selectItems = (state) => state.cart.items;
+export const selectTotal = (state) => state.cart.items.reduce((total, item) => total + item.price, 0);
+
+
+export default cartSlice.reducer;
