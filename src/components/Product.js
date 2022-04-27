2739a2d77bcaeeee39043d2997860625ab2a64ce
Biswarghya Biswas <iambiswarghya@gmail.com>
2022-04-27 23:28:29 +0530

    Product data by fakestore API

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,16 +2,35 @@ import Image from "next/image";
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../slices/cartSlice";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Product({ id, title, price, description, category, image }) {
+  const dispatch = useDispatch();
+
   const [rating] = useState(
       Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   );
 
   const [hasPrime] = useState(Math.random() < 0.5);
+
+  const addItemToCart = () => {
+    const product = {
+        id,
+        title,
+        price,
+        rating,
+        description,
+        category,
+        image,
+        hasPrime,
+    };
+
+    dispatch(addToCart(product))
+  };
 
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
@@ -40,7 +59,7 @@ function Product({ id, title, price, description, category, image }) {
             </div>
         )}
 
-        <button className="mt-auto button">Add to Cart</button>
+        <button onClick={addItemToCart} className="mt-auto button">Add to Cart</button>
     </div>
   );
 }
