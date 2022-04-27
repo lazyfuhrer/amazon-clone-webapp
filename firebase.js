0fddb98abd3d2bef2db147fdad7b3319494675f6
Biswarghya Biswas <iambiswarghya@gmail.com>
2022-04-27 23:26:54 +0530

    Firebase for login providers

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDmrYNfKtTwepW_PsDC9tcrNG9gTsb0UFs",
+    authDomain: "webapp-bbcc5.firebaseapp.com",
+    projectId: "webapp-bbcc5",
+    storageBucket: "webapp-bbcc5.appspot.com",
+    messagingSenderId: "1043401002430",
+    appId: "1:1043401002430:web:f0caa1b71db8816060fdef"
+  };
