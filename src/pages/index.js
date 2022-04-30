b3a6c09d5eab42df2dfc41f59075d42580ff62bb
Biswarghya Biswas <iambiswarghya@gmail.com>
2022-05-01 04:06:54 +0530

    Index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import ProductFeed from "../components/ProductFeed";
+import { getSession } from "next-auth/client";
 
 export default function Home({ products }) {
   return (
@@ -22,6 +23,7 @@ export default function Home({ products }) {
 }
 
 export async function getServerSideProps(context) {
+  const session = await getSession(context);
   const products = await fetch("https://fakestoreapi.com/products").then(
     (res) => res.json()
   );
@@ -29,6 +31,7 @@ export async function getServerSideProps(context) {
   return {
     props: {
       products,
+      session,
     },
   };
 }
