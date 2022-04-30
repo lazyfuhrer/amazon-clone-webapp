5f899315ddb9108e69856836c614947b7a59bb77
Biswarghya Biswas <iambiswarghya@gmail.com>
2022-05-01 04:10:12 +0530

    Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -49,7 +49,7 @@ function Product({ id, title, price, description, category, image }) {
         <p className="text-xs my-2 line-clamp-2">{description}</p>
 
         <div className="mb-5">
-            <Currency quantity={price*1.26} currency="USD"/>
+            <Currency quantity={price} currency="USD"/>
         </div>
 
         {hasPrime && (
