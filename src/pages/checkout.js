3bb7d20f46aba8e7f2a502559f41c4467aa2ba0b
Biswarghya Biswas <iambiswarghya@gmail.com>
2022-05-01 04:09:59 +0530

    Checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,11 +5,31 @@ import { useSelector } from "react-redux";
 import { selectItems , selectTotal} from "../slices/cartSlice";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/client";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const [session] = useSession();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    const checkoutSession = await axios.post('/api/create-checkout-session',
+    {
+        items: items,
+        email: session.user.email,
+    });
+
+    const result = await stripe.redirectToCheckout({
+        sessionId: checkoutSession.data.id
+    })
+
+    if (result.error) alert(result.error.message);
+  };
 
   return (
     <div className="bg-gray-100">
@@ -48,7 +68,7 @@ function Checkout() {
                             </span>  
                         </h2>
 
-                        <button disabled={!session} className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
+                        <button role="link" onClick={createCheckoutSession} disabled={!session} className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
                             {!session ? "Sign in to checkout" : "Proceed to checkout"}
                         </button>
                     </>
